28e29efa6942318222befd6f76310e052bd9ca4c
freewind <nowindlee@gmail.com>
2018-10-21 15:05:19 +0800

    demo

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-JavaScript Backend Hello World Demo
-===================================
+JavaScript Define Property Add Logging Demo
+===========================================
+
+如何使用`Object.defineProperty`来定义或者修改一个对象的属性，比如增加logging功能。
 
 ```
 npm install
 npm run demo
-```
-
-It will print:
-
-```
-Hello, Javascript
 ```
--- hello.js
+++ hello.js
@@ -1,3 +1,37 @@
-const _ = require('lodash')
+const data = {
+  name: 'javascript',
+  email: 'javascript@test.com'
+};
 
-console.log('Hello, ' + _.capitalize('javascript'))
+function enhancePropertiesWithLogging(obj) {
+  const cache = {};
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      cache[key] = obj[key];
+
+      Object.defineProperty(obj, key, {
+        get: () => { return cache[key]},
+        set: (value) => {
+          const oldValue = cache[key];
+          cache[key] = value;
+          console.log(`${key}: ${oldValue} -> ${value}`)
+        }
+      })
+    }
+  }
+}
+
+enhancePropertiesWithLogging(data);
+
+console.log('------------ before modification -------------');
+console.log(data.name);
+console.log(data.email);
+
+console.log('------------ now modification -------------');
+data.name = 'new-javascript';
+data.email = 'new-email@test.com';
+
+console.log('------------ after modification ---------------');
+console.log(data.name);
+console.log(data.email);
